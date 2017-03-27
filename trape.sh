e7429bd84a28ec4a97ebfac58215dbe6f80ba052
Rien Maertens <maertensrien@gmail.com>
2017-03-27 14:13:25 +0200

    Melding wanneer t downloaden grjid is

--- trape.sh
+++ trape.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-aflevering_name="20170320_aflevering7.mp4"
+duration=30s
+aflevering_name="20170327_aflevering8.mp4"
 playlist_url="http://vod.streamcloud.be/vier_vod/mp4:_definst_/demol/2017/volledigeafleveringen/$aflevering_name/playlist.m3u8"
 
 function test {
-    res=`curl -s -I $1 | grep HTTP/1.1 | awk {'print $2'}`
+    res=$(curl -s -I $1 | grep HTTP/1.1 | awk {'print $2'})
     if [ $res -ne 200 ]
     then
         echo "Error $res on $1"
+        return 1
     fi
 }
 
@@ -16,18 +18,22 @@ function download {
     ffmpeg -i "$1" -c copy "$2"
 }
 
-function reschedule {
-    at now + 5 minutes $1
+function loop {
+    while true; do
+        echo
+        echo "Is the episode online yet?"
+        if test "$playlist_url"; then
+            echo "Yes! downloading..."
+            download "$playlist_url" "$aflevering_name"
+            touch "$aflevering_name.downloaded"
+            break
+        else
+            echo "Nothing found. Retrying in $duration"
+            sleep "$duration"
+        fi
+    done
 }
 
-if test $playlist_url; then
-    download $playlist_url $aflevering_name
-else
-    reschedule $0
-fi
-
-
-
-download $playlist_url $aflevering_name
-
+echo "Starting loop..."
+loop
 
